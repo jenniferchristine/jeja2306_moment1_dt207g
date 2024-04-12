a47673aa2dab2a91c6dc88a75a57caaff1ca21cf
jenniferchristine <jeja2306@student.miun.se>
2024-04-12 14:26:04 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public")); // Statiska filer
+
+// Visar kurser | kurskod, kursnamn, kursplan, kursprogression, ta bort kurs |
+app.get("/", (req, res) => { // ROUTE
+    res.render("index") 
+});
+
+// Lägg till kurser i formulär | kurskod, kursnamn, kursplan, kursprogression |
+app.get("/addcourse", (req, res) => {
+    res.render("addcourse")
+});
+
+// Beskrivning av sida | syfte, databas, slutsats |
+app.get("/about", (req, res) => {
+    res.render("about") 
+});
+
+// STARTA
+app.listen(port, () => {
+    console.log("Server started on port: " + port);
+})
