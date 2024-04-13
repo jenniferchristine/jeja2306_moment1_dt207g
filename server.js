6ac2d56f78e95e5fc29da281a5056006f6a720f5
jenniferchristine <jeja2306@student.miun.se>
2024-04-13 17:04:48 +0200

    added sql to insert courses

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const port = 3000;
 const mysql = require("mysql");
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("public")); // statiska filer
+app.use(express.urlencoded({ extended: true })); // tolka post
+
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -21,29 +23,50 @@ connection.connect((err) => {
     console.log("Connected to MySql");
 });
 
-// ROUTE FÖR ATT HÄMTA DATA TILL INDEX
-app.get("/", (req, res) => {
+
+app.get("/", (req, res) => { // route för att hämta data till index
     connection.query("SELECT * FROM courses", (err, results) => {
         if (err) {
             console.error('Error fetching data from courses table: ', err);
             return;
         }
-        // Skicka kurserna till EJS-vyn för rendering
-        res.render("index", { courses: results });
+
+        res.render("index", { courses: results }); // skicka kurserna till vyn för rendering
     });
 });
 
 
-app.get("/addcourse", (req, res) => {          // Lägg till kurser i formulär | kurskod, kursnamn, kursplan, kursprogression |
-    res.render("addcourse")
+app.get("/addcourse", (req, res) => { // route för addcourse-sidan
+    res.render("addcourse");
 });
 
 
-app.get("/about", (req, res) => {             // Beskrivning av sida | syfte, databas, slutsats |
+app.post("/addcourse", (req, res) => { // hantera post-förfrågningar
+    const coursename = req.body.coursename;
+    const coursecode = req.body.coursecode;
+    const syllabus = req.body.syllabus;
+    const progression = req.body.progression;
+  
+    const sql = "INSERT INTO courses (coursename, coursecode, syllabus, progression) VALUES (?, ?, ?, ?)"; // insert till databas
+    const values = [coursename, coursecode, syllabus, progression];
+  
+    connection.query(sql, values, (err, result) => { // utför sql-frågan
+      if (err) {
+        console.error("Error inserting data: ", err);
+        res.status(500).send("Error inserting data into database");
+        return;
+      }
+      console.log("Kurs tillagd");
+      res.redirect("/"); // omdirigera till startsidan
+    });
+  });
+
+
+app.get("/about", (req, res) => {
     res.render("about") 
 });
 
-// STARTA
-app.listen(port, () => {
+
+app.listen(port, () => { // starta
     console.log("Server started on port: " + port);
-})
+});
