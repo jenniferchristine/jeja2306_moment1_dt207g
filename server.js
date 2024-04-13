af7ace35badbba5fddc5dd1935fc4b8efc172bdb
jenniferchristine <jeja2306@student.miun.se>
2024-04-13 16:43:10 +0200

    redid all code to database and reconnected

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mysql = require("mysql");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "dt207g-1",
+    password: "database123", 
+    database: "dt207g-1"
+});
 
-app.get("/", (req, res) => {                    // Visar kurser | kurskod, kursnamn, kursplan, kursprogression, ta bort kurs |
-        res.render("index");
+connection.connect((err) => {
+    if (err) {
+        console.error("Connection failed: " + err);
+        return;
+    }
+    console.log("Connected to MySql");
+});
+
+// ROUTE FÖR ATT HÄMTA DATA TILL INDEX
+app.get("/", (req, res) => {
+    connection.query("SELECT * FROM courses", (err, results) => {
+        if (err) {
+            console.error('Error fetching data from courses table: ', err);
+            return;
+        }
+        // Skicka kurserna till EJS-vyn för rendering
+        res.render("index", { courses: results });
     });
+});
 
 
 app.get("/addcourse", (req, res) => {          // Lägg till kurser i formulär | kurskod, kursnamn, kursplan, kursprogression |
